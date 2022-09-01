f492fe792e9dd70dc549e58531ae2b8ae9977434
wichudas611 <wichudas61@nu.ac.th>
2022-09-01 11:59:30 +0700

    nvwebblog

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+const UserController = require('./controllers/UserController')
+const UserAuthenController = require('./controllers/UserAuthenController')
+//const isAuthenController = require('./authen/isAuthenController') //ใช้เช็ค token
+
+
+module.exports = (app) => {
+    /* RESFUL Api for users management */
+    // create user
+    app.post('/user',
+        UserController.create
+    )
+
+    // edit user, suspend, active
+    app.put('/user/:userId',
+        UserController.put
+    )
+    // delete user
+    app.delete('/user/:userId',
+        UserController.remove
+    )
+    // get user by id
+    app.get('/user/:userId',
+        UserController.show
+    )
+
+    // get last user
+    app.get('/lastuser',
+        UserController.lastuser
+    )
+
+    // get all user
+    app.get('/users',
+        //isAuthenController,// ใช้เช็ค token
+        UserController.index
+    )
+    app.post('/login',
+        UserAuthenController.login
+    )
+}
